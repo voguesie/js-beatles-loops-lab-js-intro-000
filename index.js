b2f8538bb967a15ea21ee8ed48180d68a389e27e
Michael Vogel <voguesie@alumni.stanford.edu>
2019-12-11 01:22:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ function theBeatlesPlay(musicians, instruments) {
   }
   return whatDoTheyPlay
 }
+
+
+// Function johnLennonFacts accepts an array "facts" and returns a new array with !!! at the end of each fact.
+
+function johnLennonFacts(facts) {
+  var counter = 0;
+  while (var counter < facts.length) {
+    facts[counter] = `${facts[counter]}!!!`;
+    counter++;
+  }
+  return facts
+}
