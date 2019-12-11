d4723ecf3763e57be1a4896780624f786cfeb4ed
Michael Vogel <voguesie@alumni.stanford.edu>
2019-12-11 01:15:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add solution here
+// Function theBeatlesPlay accepts arrays "musicians" and "instruments", and returns an array of sentences stating what instrument each musician plays.
+
+function theBeatlesPlay(musicians, instruments) {
+  var whatDoTheyPlay = [];
+  for (var i = 0; i < musicians.length; i++) {
+    whatDoTheyPlay.push(`${musicians[i]} plays ${instruments[i]});
+  }
+  return whatDoTheyPlay
+}
